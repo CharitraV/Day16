bc08c30d0d609ee46a1ee8386adc963e912a507c
CharitraV <vcharitrav25@gmail.com>
2024-05-22 15:59:42 +0530

    Assignment of Day 16

---
+++ script.js
@@ -0,0 +1,35 @@
+
+setTimeout(() => {
+  document.getElementById("counter").textContent = "10";
+  setTimeout(() => {
+    document.getElementById("counter").textContent = "9";
+    setTimeout(() => {
+      document.getElementById("counter").textContent = "8";
+      setTimeout(() => {
+        document.getElementById("counter").textContent = "7";
+        setTimeout(() => {
+          document.getElementById("counter").textContent = "6";
+          setTimeout(() => {
+            document.getElementById("counter").textContent = "5";
+            setTimeout(() => {
+              document.getElementById("counter").textContent = "4";
+              setTimeout(() => {
+                document.getElementById("counter").textContent = "3";
+                setTimeout(() => {
+                  document.getElementById("counter").textContent = "2";
+                  setTimeout(() => {
+                    document.getElementById("counter").textContent = "1";
+                    setTimeout(() => {
+                      document.getElementById("counter").textContent =
+                        "Happy Independence day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
